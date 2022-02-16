47a7e1357cecbe167a805795def4bee472345b8c
Your Name <cole22simpson@gmail.com>
2022-02-15 17:11:06 -0800

    started to add an array for enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,35 +1,46 @@
+// Player variables
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-var enemyName = "Roborto";
+var playerMoney = 10;
+
+// Enemy variables
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames.length);
+for(var i = 0; i < 3; i++) {
+  console.log("apple");
+}
 var enemyHealth = 50;
 var enemyAttack = 12;
-var playerMoney = 10;
 
 var fight = function() {
 
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+    
     if (promptFight === "fight" || promptFight === "FIGHT") {
   
       enemyHealth = enemyHealth - playerAttack;
       console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+      
       if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
-      } 
+      }
       else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
       
       playerHealth = playerHealth - enemyAttack;
       console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+      
       if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
       }
-      } else {
+      else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
       }
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
+    }
+      else if (promptFight === "skip" || promptFight === "SKIP") {
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
@@ -43,5 +54,5 @@ var fight = function() {
     }
   };
   
-  fight();
+  // fight();
   
